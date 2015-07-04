815b3c2aab4e0a2d11666b28959b1b015095f24d
anna <aschughart@gmail.com>
2015-07-04 17:55:31 +0200

    Editing

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,5 +4,7 @@ from django.contrib import admin
 urlpatterns = [
   
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('blog.urls'))
+    url(r'', include('blog.urls')),
+   
+
 ]
